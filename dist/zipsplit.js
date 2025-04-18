8cddd0690a31d289dfd17cfae546f5fb7c984231
Michael Siebert <michael.siebert2k@gmail.com>
2025-04-18 17:50:51 +0200

    bugfix & restructured

--- dist/zipsplit.js
+++ dist/zipsplit.js
@@ -3,35 +3,55 @@ var someMaxUint32 = (...arg) => arg.some((x) => x === 4294967295);
 var readFileSlice = async (file, start, length) => new Promise((resolve, reject) => {
   const reader = new FileReader();
   [reader.onload, reader.onerror] = [() => resolve(
-    new Uint8Array(reader.result)
+    new DataView(reader.result)
   ), () => reject(reader.error)];
   reader.readAsArrayBuffer(file.slice(start, start + length));
 });
-var view = (bytes, size, offset = 0) => Number(
-  bytes.slice(offset, offset + size).reduce(
-    (a, b, i) => a + (BigInt(b) << BigInt(8 * i)),
-    BigInt(0)
-  )
+var readFileSliceSigned = async (file, start, length, signature, err) => {
+  const dv = await readFileSlice(file, start, length);
+  if (view(dv, 4) !== signature) throw new Error("invalid " + err);
+  return dv;
+};
+var view = (dataView, size, offset = 0) => Number(
+  size == 2 ? dataView.getUint16(offset, true) : size == 4 ? dataView.getUint32(offset, true) : dataView.getBigUint64(offset, true)
 );
-var iterateFiles = async function* enumerateZipEntries(file) {
-  const eocdSize = 42;
-  const eocd = await readFileSlice(file, file.size - eocdSize, eocdSize);
-  let centralDir = [36, 32].map((x) => view(eocd, 4, x));
-  if (view(eocd, 4, 20) !== 101010256) {
-    throw new Error("cannot locate end of central directory signature");
+var sum = (arr) => arr.reduce((a, b) => a + b, 0);
+var iterateFiles = async function* (file) {
+  const eocdSize = 22;
+  const filesize = file.size;
+  if (filesize < eocdSize) throw new Error("invalid zip file");
+  await readFileSliceSigned(file, 0, 4, 67324752, "zip file magic");
+  const eocd = await readFileSliceSigned(
+    file,
+    filesize - eocdSize,
+    eocdSize,
+    101010256,
+    "EOCD signature"
+  );
+  let cdOffsetSize = [16, 12].map((x) => view(eocd, 4, x));
+  if (someMaxUint32(...cdOffsetSize)) {
+    const zip64LocatorSize = 20;
+    const zip64EOCDHeaderBuffer = await readFileSliceSigned(
+      file,
+      view(await readFileSliceSigned(
+        file,
+        filesize - eocdSize - zip64LocatorSize,
+        zip64LocatorSize,
+        117853008,
+        "zip64 locator signature"
+      ), 8, 8),
+      56,
+      101075792,
+      "zip64 eocd signature"
+    );
+    cdOffsetSize = [48, 40].map((x) => view(zip64EOCDHeaderBuffer, 8, x));
   }
-  if (someMaxUint32(...centralDir) && view(eocd, 4) === 117853008) {
-    const eocd64 = await readFileSlice(file, view(eocd, 8, 8), 56);
-    if (view(eocd64, 4) === 101075792) {
-      centralDir = [48, 40].map((x) => view(eocd64, 8, x));
-    }
-  }
-  const cd = await readFileSlice(file, ...centralDir);
+  const cd = await readFileSlice(file, ...cdOffsetSize);
   const commentBegin = 46;
-  for (let ptr = 0, lengths; ptr + commentBegin <= cd.length; ptr += commentBegin + lengths.reduce((a, b) => a + b, 0)) {
-    if (view(cd, 4, ptr) !== 33639248) {
-      throw new Error("invalid central directory header signature");
-    }
+  for (let ptr = 0, lengths; ptr + commentBegin <= cd.byteLength; ptr += commentBegin + sum(lengths)) {
+    if (view(cd, 4, ptr) !== 33639248) throw new Error(
+      "invalid central directory file header signature"
+    );
     const tmp = [24, 20, 42].map((x) => view(cd, 4, ptr + x));
     lengths = [28, 30, 32].map((x) => view(cd, 2, ptr + x));
     const comment = [0, lengths[0]].map((x) => x + ptr + commentBegin);
@@ -50,18 +70,21 @@ var iterateFiles = async function* enumerateZipEntries(file) {
         }
       }
     }
-    if (view(cd, 2, ptr + 10) != 0 || tmp[0] != tmp[1]) {
-      throw new Error("only uncompressed files supported");
-    }
-    const lfh = await readFileSlice(file, tmp[2], tmp[2] + 30);
-    if (view(lfh, 4) != 67324752) {
-      throw new Error("invalid local file header signature");
-    }
+    if (view(cd, 2, ptr + 10) !== 0 || tmp[0] != tmp[1]) throw new Error(
+      `archive contains compressed file, only uncompressed supported`
+    );
+    const localHeader = await readFileSliceSigned(
+      file,
+      tmp[2],
+      30,
+      67324752,
+      "local file header signature"
+    );
     yield new File(
-      [((x) => file.slice(x, x + tmp[0]))([26, 28].map(
-        (x) => view(lfh, 2, x)
-      ).reduce((a, b) => a + b, 30 + tmp[2]))],
-      new TextDecoder("utf-8").decode(cd.slice(...comment))
+      [((localLengthsSum) => file.slice(...[0, tmp[0]].map(
+        (x) => x + tmp[2] + 30 + localLengthsSum
+      )))(sum([26, 28].map((x) => view(localHeader, 2, x))))],
+      new TextDecoder("utf-8").decode(cd.buffer.slice(...comment))
     );
   }
 };
--- src/zipsplit.ts
+++ src/zipsplit.ts
@@ -1,53 +1,72 @@
 const someMaxUint32 = (...arg: number[]) => arg.some(x => x === 0xffffffff);
 
-const readFileSlice = async (file: File, start: number, length: number) => 
-    new Promise<Uint8Array>((resolve, reject) => {
+const readFileSlice = async (file: File, start: number, length: number) =>
+    new Promise<DataView>((resolve, reject) => {
         const reader = new FileReader();
         [reader.onload, reader.onerror] = [() => resolve(
-            new Uint8Array(reader.result as ArrayBuffer)
+            new DataView(reader.result as ArrayBuffer)
         ), () => reject(reader.error)];
         reader.readAsArrayBuffer(file.slice(start, start + length));
     });
 
-const view = (bytes: Uint8Array, size: number, offset = 0) => Number(
-    bytes.slice(offset, offset + size).reduce(
-        (a, b, i) => a + (BigInt(b) << BigInt(8*i)), BigInt(0)
-    )
-);
+const readFileSliceSigned = async (
+    file: File, start: number, length: number, signature: number, err: string
+) => {
+    const dv = await readFileSlice(file, start, length);
+    if(view(dv, 4) !== signature) throw new Error('invalid ' + err);
+    return dv;
+};
 
-export const iterateFiles = async function* enumerateZipEntries(file: File) {
-    const eocdSize = 42;
-    const eocd = await readFileSlice(file, file.size - eocdSize, eocdSize);
-    // [centralDirOffset, centralDirSize]
-    let centralDir = [36, 32].map(x => view(eocd, 4, x));
+const view = (dataView: DataView, size: 2 | 4 | 8, offset = 0) => Number(
+    size == 2 ? dataView.getUint16(offset, true) :
+    size == 4 ? dataView.getUint32(offset, true) :
+    dataView.getBigUint64(offset, true)
+)
 
-    if (view(eocd, 4, 20) !== 0x06054b50) { // EOCD_SIG
-        throw new Error("cannot locate end of central directory signature");
+const sum = (arr: number[]) => arr.reduce((a, b) => a + b, 0);
+
+export const iterateFiles = async function* (file: File) {
+    const eocdSize = 22;
+    const filesize = file.size;
+
+    if (filesize < eocdSize) throw new Error('invalid zip file');
+
+    await readFileSliceSigned(file, 0, 4, 0x04034B50, 'zip file magic');
+
+    const eocd = await readFileSliceSigned(
+        file, filesize - eocdSize, eocdSize, 0x06054b50, 'EOCD signature'
+    );
+
+    let cdOffsetSize = [16, 12].map(x => view(eocd, 4, x));
+
+    if (someMaxUint32(...cdOffsetSize)) {
+        const zip64LocatorSize = 20;
+
+        const zip64EOCDHeaderBuffer = await readFileSliceSigned(
+            file, view(await readFileSliceSigned(
+                file, filesize - eocdSize - zip64LocatorSize,
+                zip64LocatorSize, 0x07064b50, 'zip64 locator signature'
+            ), 8, 8), 56, 0x06064b50, 'zip64 eocd signature'
+        );
+
+        cdOffsetSize = [48, 40].map(x => view(zip64EOCDHeaderBuffer, 8, x));
     }
 
-    // 0x07064b50: ZIP64_LOCATOR_SIG
-    if (someMaxUint32(...centralDir) && view(eocd, 4) === 0x07064b50) {
-        const eocd64 = await readFileSlice(file, view(eocd, 8, 8), 56);
-        if (view(eocd64, 4) === 0x06064b50) { // ZIP64_EOCD_SIG
-            centralDir = [48, 40].map(x => view(eocd64, 8, x));
-        }
-    }
-
-    const cd = await readFileSlice(file, ...centralDir as [number, number]);
+    const cd = await readFileSlice(file, ...cdOffsetSize as [number, number]);
 
     const commentBegin = 46;
     for (
-        let ptr = 0, lengths; ptr + commentBegin <= cd.length;
-        ptr += commentBegin + lengths.reduce((a, b) => a + b, 0)
+        let ptr = 0, lengths; ptr + commentBegin <= cd.byteLength;
+        ptr += commentBegin + sum(lengths)
     ) {
-        if (view(cd, 4, ptr) !== 0x02014b50) {
-            throw new Error("invalid central directory header signature");
-        }
+        if (view(cd, 4, ptr) !== 0x02014b50) throw new Error(
+            'invalid central directory file header signature'
+        );
 
         // [uncompressedSize, compressedSize, localHeaderOffset]
         const tmp = [24, 20, 42].map(x => view(cd, 4, ptr + x));
 
-        // [fileNameLength, extraFieldLength, fileCommentLength]
+        // [filenameLength, extraFieldLength, fileCommentLength]
         lengths = [28, 30, 32].map(x => view(cd, 2, ptr + x));
         const comment = [0, lengths[0]].map(x => x + ptr + commentBegin);
 
@@ -70,20 +89,19 @@ export const iterateFiles = async function* enumerateZipEntries(file: File) {
         }
 
         // compression method
-        if (view(cd, 2, ptr + 10) != 0 || tmp[0] != tmp[1]) {
-            throw new Error("only uncompressed files supported");
-        }
+        if(view(cd, 2, ptr + 10) !== 0 || tmp[0] != tmp[1]) throw new Error(
+            `archive contains compressed file, only uncompressed supported`
+        );
 
-        const lfh = await readFileSlice(file, tmp[2], tmp[2] + 30);
-
-        if(view(lfh, 4) != 0x04034b50) {
-            throw new Error('invalid local file header signature')
-        }
+        const localHeader = await readFileSliceSigned(
+            file, tmp[2], 30, 0x04034b50, 'local file header signature'
+        );
 
         yield new File(
-            [(x => file.slice(x, x + tmp[0]))([26, 28].map(
-                x => view(lfh, 2, x)).reduce((a, b) => a + b, 30 + tmp[2]))],
-            new TextDecoder("utf-8").decode(cd.slice(...comment))
+            [(localLengthsSum => file.slice(...[0, tmp[0]].map(
+                x => x + tmp[2] + 30 + localLengthsSum
+            )))(sum([26, 28].map(x => view(localHeader, 2, x))))],
+            new TextDecoder("utf-8").decode(cd.buffer.slice(...comment))
         );
     }
 }
